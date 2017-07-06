c586c92cd687ce1cc8d2367aeff6e1018b793aa2
WxHlin <wei18627055053@sina.com>
2017-07-06 14:47:36 +0800

    Initial commit

---
+++ app/src/main/java/com/xw/tools/MainActivity.java
@@ -0,0 +1,19 @@
+package com.xw.tools;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xw.tools.tools.L;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        L.d("hhhh","uuuuuuu");
+        L.d("pppp");
+
+
+    }
+}
